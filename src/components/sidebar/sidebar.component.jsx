30ce97b21111290291814e8fe3df47278c832229
Talal Anwar <talal.anwar@icommunix.com>
2021-05-02 13:05:47 +0500

    completed sidebar design.

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -2,11 +2,21 @@ import { Avatar } from "@material-ui/core";
 import "./sidebar.styles.scss";
 
 function Sidebar() {
+	const recentItem = (topic) => (
+		<div className="recent-item">
+			<span className="hash">#</span>
+			<p>{topic}</p>
+		</div>
+	);
+
 	return (
 		<div className="sidebar">
 			{/* top section for personal details */}
 			<div className="top">
-				<img src="../../assets/images/sidebar_top_bg.jpg" alt="" />
+				<img
+					src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixlib=rb-1.2.1&ixid=MXwxMjA3f088MHxleHBsb3JlLWZlZWR8MXx8fGVufDBBfHw%3D&w=100&q=80"
+					alt=""
+				/>
 				<Avatar className="avatar" />
 				<h2>Talal Anwar</h2>
 				<h4>talal.anwar.warraich@gmail.com</h4>
@@ -22,9 +32,14 @@ function Sidebar() {
 					<p className="number">2,600</p>
 				</div>
 			</div>
-			{/* sidebar button */}
-			<div className="button">
+			{/* sidebar bottom */}
+			<div className="bottom">
 				<p>Recent</p>
+				{recentItem("reactjs")}
+				{recentItem("programming")}
+				{recentItem("softwareengineering")}
+				{recentItem("javascript")}
+				{recentItem("angular")}
 			</div>
 		</div>
 	);
